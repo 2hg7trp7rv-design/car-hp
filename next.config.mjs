349f18067645c73053e30aeae3d331dfcd2d0d50
2hg7trp7rv-design <2hg7trp7rv@privaterelay.appleid.com>
2025-11-26 07:05:56 +0900

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -2,6 +2,8 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
+    // CloudflareではNextの画像最適化APIが使えないので無効化
+    unoptimized: true,
     remotePatterns: [
       {
         protocol: "https",
