686da3b0711b020569b1c85004561fb55093fd5a
2hg7trp7rv-design <2hg7trp7rv@privaterelay.appleid.com>
2025-11-26 07:21:21 +0900

    Update page.tsx

--- app/news/page.tsx
+++ app/news/page.tsx
@@ -3,6 +3,8 @@ import Link from "next/link";
 import { getLatestNews } from "@/lib/news";
 import { GlassCard } from "@/components/GlassCard";
 
+export const runtime = "edge";
+
 export const metadata = {
   title: "ニュース一覧 | CAR BOUTIQUE",
   description:
