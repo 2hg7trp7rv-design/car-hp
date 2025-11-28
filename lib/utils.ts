093ff0ee591a95b9f5ffeba2ad2b3a303e7845dd
2hg7trp7rv-design <2hg7trp7rv@privaterelay.appleid.com>
2025-11-28 17:16:47 +0900

    Update utils.ts

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,1 +1,9 @@
-
+// lib/utils.ts
+export function cn(
+  ...inputs: Array<string | number | false | null | undefined>
+): string {
+  return inputs
+    .filter((v) => typeof v === "string" || typeof v === "number")
+    .map((v) => String(v))
+    .join(" ");
+}
