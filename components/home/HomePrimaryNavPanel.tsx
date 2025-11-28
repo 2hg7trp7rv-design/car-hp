4ae7e859da05c3c5692bc29b4f5a7d58db9c6ff2
2hg7trp7rv-design <2hg7trp7rv@privaterelay.appleid.com>
2025-11-28 22:29:37 +0900

    Update HomePrimaryNavPanel.tsx

--- components/home/HomePrimaryNavPanel.tsx
+++ components/home/HomePrimaryNavPanel.tsx
@@ -3,8 +3,9 @@
 
 /**
  * 以前の HOME 上部ナビ（NEWS / CARS / COLUMN / GUIDE）用コンポーネント。
- * いまは使わないので、既存の import を壊さないためのダミー実装。
+ * 現在は使わないので、既存の import を壊さないためのダミー実装にしておく。
  */
 export default function HomePrimaryNavPanel() {
+  // 何も描画しない = 上の大きなカードは出てこない
   return null;
 }
