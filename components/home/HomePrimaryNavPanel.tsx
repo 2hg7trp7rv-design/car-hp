191ddb857417551b59912ee2efe51b7a3a62dc04
2hg7trp7rv-design <2hg7trp7rv@privaterelay.appleid.com>
2025-11-29 00:12:29 +0900

    Update HomePrimaryNavPanel.tsx

--- components/home/HomePrimaryNavPanel.tsx
+++ components/home/HomePrimaryNavPanel.tsx
@@ -1,11 +1,10 @@
 // components/home/HomePrimaryNavPanel.tsx
-"use client";
+// 以前はヒーローの上に大きく表示されていた NEWS/CARS/COLUMN/GUIDE パネル。
+// 役割を HomeNavDrawer に移したので、ここでは一切描画しないダミーにする。
 
-/**
- * 以前の HOME 上部ナビ（NEWS / CARS / COLUMN / GUIDE）用コンポーネント。
- * 現在は使わないので、既存の import を壊さないためのダミー実装にしておく。
- */
-export default function HomePrimaryNavPanel() {
-  // 何も描画しない = 上の大きなカードは出てこない
+export function HomePrimaryNavPanel() {
   return null;
 }
+
+// 名前付き import でも default import でも、どちらでも null を返すようにしておく
+export default HomePrimaryNavPanel;
