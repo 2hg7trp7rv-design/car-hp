9c77eb14a66a5cb16646155cf87be6e1c77bd2c4
2hg7trp7rv-design <2hg7trp7rv@privaterelay.appleid.com>
2026-06-23 06:49:13 +0900

    chore: open column design preview from root

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,8 @@
-"use client";
+import { redirect } from "next/navigation";
 
-import Navigation from "@/app/components/Navigation";
-import HeroSection from "@/app/sections/HeroSection";
-import FooterSection from "@/app/sections/FooterSection";
-import HomeEditorialSequence from "@/app/sections/HomeEditorialSequence";
+const DESIGN_PREVIEW_PATH =
+  "/column/modern-car-custom-regret-reason-column";
 
 export default function Home() {
-  return (
-    <main className="relative min-h-screen overflow-hidden bg-[#0A0A0A] text-white">
-      <Navigation />
-      <HeroSection />
-      <HomeEditorialSequence />
-      <FooterSection />
-    </main>
-  );
+  redirect(DESIGN_PREVIEW_PATH);
 }
